8b374ed91782d8f3dd6b33a983f3ba2d8981fcb1
Prince Junior Mguni <prince.mguni@outlook.com>
2022-03-14 08:45:52 +0200

    created data store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,40 @@
 import * as data from "@/assets/Data.json";
 import { defineStore } from "pinia";
 
+interface User {
+  firstName: string;
+  lastName: string;
+  email: string;
+  id: number;
+}
+
 export const dataStore = defineStore("main", {
   state: () => ({
     data,
+    currentId: data[0].account_id,
+    currentUser: data[0],
   }),
   getters: {
-    getData: (state) => {
-      {
-        state.data;
-      }
+    getUsers: (state) => {
+      let obj: User[] = [];
+      state.data.forEach((element) => {
+        obj.push({
+          firstName: element.first_name,
+          lastName: element.last_name,
+          email: element.email_address,
+          id: element.account_id,
+        });
+      });
+      return obj;
+    },
+  },
+  actions: {
+    setCurrentId(id: number) {
+      this.currentId = id;
+    },
+    setCurrentUser(id: number) {
+      let obj = data.find((o) => o.account_id === id);
+      Object.assign(this.currentUser, obj);
     },
   },
 });
